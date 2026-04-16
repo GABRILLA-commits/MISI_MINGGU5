90f27ce0a18f0c1f4757ee2abfde06b6e51d9cfa
GABRILLA-commits <2411102441174@umkt.ac.id>
2026-04-16 23:58:07 +0800

    clean code

--- tugas5.py
+++ tugas5.py
@@ -1,9 +1,20 @@
-def h(a, b, c):
-    d = a + b + c
-    print("Total:", d)
-    if d > 100000:
-        print("Diskon 10%")
-        d = d - (d * 0.1)
-    print("Bayar:", d)
+DISCOUNT_THRESHOLD = 100000
+DISCOUNT_RATE = 0.1
 
-h(50000, 30000, 40000)
+def calculate_total(price1, price2, price3):
+    return price1 + price2 + price3
+
+def apply_discount(total):
+    if total > DISCOUNT_THRESHOLD:
+        return total - (total * DISCOUNT_RATE)
+    return total
+
+def print_result(total, final_total):
+    print("Total:", total)
+    if total != final_total:
+        print("Diskon 10% diterapkan")
+    print("Bayar:", final_total)
+
+total = calculate_total(50000, 30000, 40000)
+final_total = apply_discount(total)
+print_result(total, final_total)
